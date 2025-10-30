ce0969ddbcbd1e4d3707ce2da40902d6b237ab6e
Noa Mokady <noa.mokady@post.runi.ac.il>
2025-10-30 16:45:35 +0200

    Finished exercise 4

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,14 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) (Math.random()*lim);
+		int b = (int) (Math.random()*lim);
+		int c = (int) (Math.random()*lim);
+		System.out.println(String.format("%s %s %s", a, b, c));
+		int minNum = Math.min(a, Math.min(b, c));
+		int maxNum = Math.max(a, Math.max(b, c));
+		int mediumNum = a + b + c - minNum - maxNum;
+		System.out.println(String.format("%s %s %s", minNum, mediumNum, maxNum));
 	}
 }
